7c358d25db943b18bf196e857ab8802e8cba746a
CuboidRaptor <fanjas112358@gmail.com>
2022-06-21 14:59:12 -0400

    comments and changes
    comments and changes

--- docs/js/aload.js
+++ docs/js/aload.js
@@ -2,5 +2,5 @@ $(window).on("load", function() {
 	if ($("#trailer").length <= 0) {
 		return;
 	}
-	$("#trailer").attr("src", "https://www.youtube.com/embed/4EXEc-bec-s");
+	$("#trailer").attr("src", "https://www.youtube-nocookie.com/embed/4EXEc-bec-s");
 });
