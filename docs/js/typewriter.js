36373232a6df7d106c8f90fbbcae95c66cffc6ef
CuboidRaptor <fanjas112358@gmail.com>
2022-09-01 14:16:52 -0400

    i'm not minitrue, I promise

--- docs/js/typewriter.js
+++ docs/js/typewriter.js
@@ -1,14 +1,14 @@
 // Defining constants
 var quoteArray = [
-	"I'm a dumb guy who makes youtube videos.",
-	"I'm also a nerd who writes programs.",
+	"Hi, I'm a dumb guy who makes youtube videos.",
+	"I'm also a nerd who writes code.",
 	"Sometimes my programs even turn out to be useful.",
-	"I play all sorts of stuff on my youtube channel, ^^^^such as Minecraft, Undertale, Celeste, etc.",
+	"I play all sorts of stuff on my youtube channel, ^^^^such as Minecraft, Undertale, Minecraft, Minecraft, etc.",
 	"Please subscribe, ^^^^^or something.",
 	"Oh wait, ^^^I didn't give you the link, did I.",
 	"So, ^^^^it's in the sidebar that just so happens to be at the top-left of this page.",
 	"Have you subscribed? ^^No? ^^Yeah, that's about what I expected.",
-	"Anyways, ^^^^^I guess the clever piece of JavaScript in this page that is typing my text can loop back to the beginning now."
+	"Anyways, ^^^^^I guess the clever piece of JavaScript in this page that is typing this text can loop back to the beginning now."
 ];
 var textPosition = 0; 
 var mode = "w";
