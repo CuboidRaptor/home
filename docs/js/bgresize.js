7ed41eb4d7b98a56352f8185c444b24b093d3d0e
Cuboid <fanjas112358@gmail.com>
2024-09-23 12:05:35 -0400

    more cleaning
    and updated modern-normalize.css

--- docs/js/bgresize.js
+++
@@ -1,4 +0,0 @@
-// screw up the iframe content so it displays properly and also clips off whatever emoji bug was in nicky's code somehow
-bgobj = document.getElementById("bgthingy")
-bgobj.width = window.screen.width + 8;
-bgobj.height = window.screen.height + 0;
